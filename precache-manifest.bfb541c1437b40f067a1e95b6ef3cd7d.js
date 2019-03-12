a620db119709a6893b5660b0e6cbe9f09fb4b466
baohe-zhang <baohez2@illinois.edu>
2019-03-11 22:34:55 -0500

    Updates

---
+++ precache-manifest.bfb541c1437b40f067a1e95b6ef3cd7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cc61d821af37f54d0c3",
+    "url": "/movie-query/static/js/runtime~main.913d1c51.js"
+  },
+  {
+    "revision": "18b316e1d8a4a83b8f3e",
+    "url": "/movie-query/static/js/main.36aa0638.chunk.js"
+  },
+  {
+    "revision": "c22fb9275dea5dbd0d2a",
+    "url": "/movie-query/static/js/2.8795cc4e.chunk.js"
+  },
+  {
+    "revision": "18b316e1d8a4a83b8f3e",
+    "url": "/movie-query/static/css/main.605cda30.chunk.css"
+  },
+  {
+    "revision": "c22fb9275dea5dbd0d2a",
+    "url": "/movie-query/static/css/2.254a3adf.chunk.css"
+  },
+  {
+    "revision": "82938036ef9e1863baf753e8f29712a2",
+    "url": "/movie-query/index.html"
+  }
+];
